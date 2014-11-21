e4efa9e03496e72d778bf9f02dbae7f9131eebe9
Liam Mitchell <hockey_crazy_08@yahoo.com>
2014-11-21 15:59:48 -0700

    Updated request spec to include example JSON

--- php/recipes/search.php
+++ php/recipes/search.php
@@ -1,4 +1,27 @@
-<?php
+<?php   
+function add_param(&$query, &$operator, $attribute, $value)
+{
+    $query .= $operator;
+    if ($operator == " WHERE ") {
+        $operator = " AND ";
+    }
+
+    $query .= $attribute;
+    $query .= " = ";
+    $query .= $value;
+    $query .= " ";
+}
+
+function build_query()
+{
+    $ret = "SELECT * FROM recipe";
+    $operator = " WHERE ";
+
+    if (isset($_GET['author_name'])) {
+        add_param($ret, $operator, 'author_name', $_GET['author_name']);
+    }   
+}
+
 $mysqli = new mysqli("localhost", "sql", "sql-pwd", "recipedb");
 
 if ($mysqli->connect_errno) {
@@ -6,6 +29,8 @@ if ($mysqli->connect_errno) {
     echo $mysqli->connect_errno;
     exit();
 }
+
+$query = "SELECT * FROM recipe";
 
 $recipes = $mysqli->query("SELECT * FROM recipe");
 

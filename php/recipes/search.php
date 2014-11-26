490a3eaed2c4d803b82543da79b7513880f8ed82
Liam Mitchell <hockey_crazy_08@yahoo.com>
2014-11-25 19:13:06 -0700

    Moved diagrams into doc/ and updated recipes/search script. Search script now sucessfully accepts parameters:   name   author_name   rating   sort_by

--- php/recipes/search.php
+++ php/recipes/search.php
@@ -4,15 +4,60 @@ $attributes = [
                "name",
                "author_name",
                "rating",
-               "dietary_restriction"
                ];
 
 $modifiers = [
               "show_only",
-              "sort_by",
+              "dietary_restriction"
               ];
 
-$mysqli = new mysqli("localhost", "sql", "sql-pwd", "recipedb");
+function order_query($query)
+{
+    $clause = "";
+    foreach ($_GET as $param => $value) {
+        if ($param == "sort_by") {
+            $clause = " ORDER BY " . $value;
+        }
+    }
+
+    return $query . $clause;
+}
+
+function create_query($attributes)
+{
+    $query = "SELECT * FROM recipe";
+    $clause = " WHERE ";
+
+    foreach ($_GET as $param => $value) {
+        if (in_array($param, $attributes)) {
+            if ($param != "rating") {
+                $value = "\"" . $value . "\"";
+            }
+            
+            $query .= $clause . $param . "=" . $value;
+            $clause = " AND ";
+        }
+    }
+
+    return order_query($query);
+}
+
+function filter_results($results, $modifiers)
+{
+    $show_only = NULL;
+    $dietary_restriction = [];
+    
+    foreach ($_GET as $param => $value) {
+        if ($param == "show_only") {
+            $show_only = $value;
+        }
+
+    }
+
+    return $results;
+}
+
+$mysqli = new mysqli("localhost", "mysql", "mysql", "recipedb");
 
 if ($mysqli->connect_errno) {
     echo "Error connecting to database:";
@@ -20,18 +65,23 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-$query = "SELECT * FROM recipe";
-
-$recipes = $mysqli->query("SELECT * FROM recipe");
+echo "creating query...<br>";
+$query = create_query($attributes);
+echo "created query: ";
+echo $query;
+echo "<br>";
+$result = $mysqli->query($query);
+echo "ran query!<br>";
+$recipes = filter_results($result, $modifiers);
+echo "filtered results!<br>";
 
 if ($recipes) {
     while ($recipe = $recipes->fetch_assoc()) {
-        printf("Recipe:<br>");
         printf(json_encode($recipe));
-        printf("<br>");
     }
 }
 else {
     printf("No rows retrieved!\n");
 }
+
 ?>

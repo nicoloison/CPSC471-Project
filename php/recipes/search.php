5ffbc2c269dd146254af59aba88f9d75c5d8a8d6
Liam Mitchell <hockey_crazy_08@yahoo.com>
2014-11-26 15:13:50 -0700

    Updated request spec as follows:   Change create recipe and cookbook requests to use multipart/form-data.    This allows for the POSTing of images along with the rest of the form    data (though it makes things a little more complicated).   Change search scripts to use rating and prep_time as minimum values, and    add corresponding rating_max and prep_time_max options.
    Update recipes/search script to support the following options:
      instructions
      prep_time
      prep_time_max
      rating_max
      description
      using
      using_only
      show_only
      sort_by
    
    Update recipes/search script to perform a text search on appropriate text
    fields (name, author_name, instructions, description) instead of taking
    only exact matches.

--- php/recipes/search.php
+++ php/recipes/search.php
@@ -3,43 +3,153 @@
 $attributes = [
                "name",
                "author_name",
+               "instructions",
+               "prep_time",
+               "prep_time_max",
                "rating",
+               "rating_max",
+               "description",
+               "using",
+               "using_only",
                ];
 
 $modifiers = [
               "show_only",
-              "dietary_restriction"
+              "sort_by",
+              "dietary_restriction",
               ];
 
-function order_query($query)
+function using_clause($operator, $ingredient)
+{
+    return $operator . " EXISTS (SELECT * FROM recipe_ingredient WHERE ("
+        . "name = recipe_name AND ingredient_name = '" . $ingredient . "'))";
+}
+
+/*
+ * WHERE NOT EXISTS (SELECT * FROM recipe_ingredient
+ *                   WHERE name = recipe_name
+ *                   AND (ingredient_name <> ingredient1
+ *                        AND ingredient_name <> ingredient2
+ *                        AND ingredient_name <> ingredient3
+ *                   ))
+ */
+function using_only_clause($operator, $ingredients)
+{
+    echo "using_only:".implode(" ", $ingredients);
+    $clause = $operator . " NOT EXISTS (SELECT * FROM recipe_ingredient WHERE "
+        . "name = recipe_name AND ";
+
+    while (count($ingredients) > 1) {
+        $clause .= "ingredient_name <> '" . array_shift($ingredients) . "' AND ";
+    }
+
+    $clause .= "ingredient_name <> '" . $ingredients[0] . "') ";
+    echo "using_only clause():" . $clause;
+    return $clause;
+}
+
+function dietary_restriction_clause()
+{
+
+}
+
+function less_clause($operator, $field, $value)
+{
+    return $operator . " " . $field . "<" . $value;
+}
+
+function greater_clause($operator, $field, $value)
+{
+    return $operator . " " . $field . ">" . $value;
+}
+
+function equals_clause($operator, $field, $value)
+{
+    return $operator . " " . $field . "=" . $value;
+}
+
+function like_clause($operator, $field, $value)
+{
+    return $operator . " " . $field
+        . " LIKE CONCAT ('%', '" . $value . "', '%')";
+}
+
+function order_by_clause()
 {
     $clause = "";
+
     foreach ($_GET as $param => $value) {
         if ($param == "sort_by") {
+            $desc = false;
+
+            if ($value[0] == '-') {
+                $desc = true;
+                $value = substr($value, 1);                
+            }
+
             $clause = " ORDER BY " . $value;
+
+            if ($desc) {
+                $clause .= " DESC";
+            }
+
+            break;
         }
     }
 
-    return $query . $clause;
+    return $clause;
+}
+
+function limit_clause()
+{
+    $clause = "";
+
+    if (array_key_exists("show_only", $_GET)) {
+        $clause = " LIMIT " . $_GET["show_only"];
+    }
+
+    return $clause;
+}
+
+function where_clause($attributes)
+{
+    $operator = " WHERE ";
+    $clause = "";
+
+    foreach ($_GET as $param => $value) {
+        if (in_array($param, $attributes)) {
+            if ($param == "rating" || $param == "prep_time") {
+                $clause .= greater_clause($operator, $param, $value);
+            }
+            else if (strpos($param, "_max") !== false) {
+                $param = substr($param, 0, -4);
+                $clause .= less_clause($operator, $param, $value);
+            }
+            else if ($param == "using") {
+                $clause .= using_clause($operator, $value);
+            }
+            else if ($param == "using_only") {
+                $clause .= using_only_clause($operator, $value);
+            }
+            else {
+                $clause .= like_clause($operator, $param, $value);
+            }
+
+            $operator = " AND ";
+        }
+    }
+
+    return $clause;
 }
 
 function create_query($attributes)
 {
     $query = "SELECT * FROM recipe";
-    $clause = " WHERE ";
+    $query .= where_clause($attributes);
+    $query .= order_by_clause();
+    $query .= limit_clause();
 
-    foreach ($_GET as $param => $value) {
-        if (in_array($param, $attributes)) {
-            if ($param != "rating") {
-                $value = "\"" . $value . "\"";
-            }
-            
-            $query .= $clause . $param . "=" . $value;
-            $clause = " AND ";
-        }
-    }
-
-    return order_query($query);
+    return $query;
 }
 
 function filter_results($results, $modifiers)
@@ -51,7 +161,6 @@ function filter_results($results, $modifiers)
         if ($param == "show_only") {
             $show_only = $value;
         }
-
     }
 
     return $results;
@@ -65,15 +174,11 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-echo "creating query...<br>";
 $query = create_query($attributes);
-echo "created query: ";
-echo $query;
-echo "<br>";
 $result = $mysqli->query($query);
-echo "ran query!<br>";
 $recipes = filter_results($result, $modifiers);
-echo "filtered results!<br>";
+
+echo "ran query: " . $query;
 
 if ($recipes) {
     while ($recipe = $recipes->fetch_assoc()) {
